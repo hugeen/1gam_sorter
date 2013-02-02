8afbe85d272493f4b3a84f00d131a776fa7b9577
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-02-02 18:05:52 +0100

    collect games

--- 1gam-sorter.js
+++ 1gam-sorter.js
@@ -1,5 +1,5 @@
 (function() {
-    
+
     var s = document.createElement('script');
     s.setAttribute('src', 'http://code.jquery.com/jquery-1.9.0.min.js');
     document.getElementsByTagName('body')[0].appendChild(s);
@@ -7,24 +7,48 @@
     var s = document.createElement('script');
     s.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js');
     document.getElementsByTagName('body')[0].appendChild(s);
-    
+
     var s = document.createElement('link');
     s.setAttribute('href', 'http://twitter.github.com/bootstrap/assets/css/bootstrap.css');
     s.setAttribute('rel', 'stylesheet');
     s.setAttribute('type', 'text/css');
     document.getElementsByTagName('head')[0].appendChild(s);
-    
+
     var s = document.createElement('link');
     s.setAttribute('href', 'http://hugeen.github.com/1gam_sorter/1gam-sorter.css');
     s.setAttribute('rel', 'stylesheet');
     s.setAttribute('type', 'text/css');
     document.getElementsByTagName('head')[0].appendChild(s);
-    
-    
-    $(function() {
-       
-       $("body").attr({ "ng-app": "sorter" });   
+
+
+    window.onload = function() {
         
-    });
-    
+        var module = angular.module('sorter', []);
+        
+        $("body").attr({
+            "ng-app": "sorter"
+        });
+        
+        var games = [];
+        
+        $(".gadiv").each(function() {
+            var div = $(this);
+            games.push({
+                div: div,
+                name: div.find(".ganame").text(),
+                description: div.find(".gabyli").text(),
+                credits: div.find(".gacred").text(),
+                tags: div.find(".gatags").text(),
+                about: div.find(".gabout").text(),
+                author: div.find(".gauser a").text()
+            });
+        });
+        
+        console.log(games);
+        window.sorterCtrl = function($scope) {
+            
+        }
+        
+    }
+
 })();

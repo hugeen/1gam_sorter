1e80d1dd9876456fececb90acb8eef5a1e0222ad
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-02-02 17:41:45 +0100

    sorter initialized

---
+++ 1gam-sorter.js
@@ -0,0 +1,23 @@
+(function() {
+    
+    var s = document.createElement('script');
+    s.setAttribute('src', 'http://code.jquery.com/jquery-1.9.0.min.js');
+    document.getElementsByTagName('body')[0].appendChild(s);
+
+    var s = document.createElement('script');
+    s.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js');
+    document.getElementsByTagName('body')[0].appendChild(s);
+    
+    var s = document.createElement('link');
+    s.setAttribute('href', 'http://twitter.github.com/bootstrap/assets/css/bootstrap.css');
+    s.setAttribute('rel', 'stylesheet');
+    s.setAttribute('type', 'text/css');
+    document.getElementsByTagName('head')[0].appendChild(s);
+    
+    var s = document.createElement('link');
+    s.setAttribute('href', 'http://hugeen.github.com/1gam_sorter/1gam-sorter.css');
+    s.setAttribute('rel', 'stylesheet');
+    s.setAttribute('type', 'text/css');
+    document.getElementsByTagName('head')[0].appendChild(s);
+
+})();
